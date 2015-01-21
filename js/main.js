957661edb170b23546e820e9b7e5350031cfed66
iheartkode <markhjr@outlook.com>
2015-01-20 23:19:45 -0800

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$('button').on('click', function() {
+    
+    var find = $('#search').val(); 
+    
+    $('#search').val("");
+
+      
+        var users = $("ul");
+        Gh3.Users.search(find, {start_page : 1}, function (err, response) {
+          if(err) {
+            throw "Error Couldn't fetch Users"
+          }
+          response.each(function (user) {
+            users.append(
+              $('<li>').append(' <a href="https://www.github.com/' + user.login + ' ">  ' + user.login + ' </a> ' + '<br> Name: ' + user.name + '<br> Language: ' + user.language + '<br><br>')
+          ); //end append
+        }); // end response function
+      }); // end gh3 user function
+  
+ }); // end button on click
+
